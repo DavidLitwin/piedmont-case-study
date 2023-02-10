0f4c83e13410843be4663a1374e84bbce0cafe5f
David Litwin <davidglitwin@posteo.net>
2023-02-10 17:00:48 -0500

    new figs and logit

--- ch3_plots-saturation.py
+++ ch3_plots-saturation.py
@@ -7,17 +7,17 @@ import pickle
 import glob
 import rasterio as rd
 from sklearn.linear_model import LogisticRegression
+import statsmodels.formula.api as smf
 from scipy.ndimage import gaussian_filter
 
 import matplotlib.pyplot as plt
 from matplotlib import cm
 from matplotlib import colors
 
-
+save_directory = 'C:/Users/dgbli/Documents/Papers/Ch3_oregon_ridge_soldiers_delight/figures/'
 
 # %% paths
 
-save_directory = 'C:/Users/dgbli/Documents/Papers/Ch3_oregon_ridge_soldiers_delight/figures/'
 
 ## Soldiers Delight:
 path = 'C:/Users/dgbli/Documents/Research/Soldiers Delight/data/'
@@ -230,7 +230,7 @@ for i in range(len(paths)):
     ax.set_xlabel('TI')
     ax.set_title(df.BeginTime[1][0:10])
     fig.colorbar(sc, label='curvature')
-    plt.savefig(f'C:/Users/dgbli/Documents/Papers/Ch3_oregon_ridge_soldiers_delight/figures/DruidRun_sat_TI_{df.BeginTime[1][0:10]}.png')
+    # plt.savefig(f'C:/Users/dgbli/Documents/Papers/Ch3_oregon_ridge_soldiers_delight/figures/DruidRun_sat_TI_{df.BeginTime[1][0:10]}.png')
     # plt.savefig(f'C:/Users/dgbli/Documents/Papers/Ch3_oregon_ridge_soldiers_delight/figures/BaismanRun_sat_TI_{df.BeginTime[1][0:10]}.png')
     plt.show()
 
@@ -287,7 +287,7 @@ curvfile = "LSDTT/baltimore2015_DR1_CURV.bil" # Druids Run
 # add filtered TI points
 tis = rd.open(path+TIfile)
 coords = [(x,y) for x, y in zip(dfall['X'], dfall['Y'])]
-dfall['TI_filtered'] = [x for x in tis.sample(coords)]
+dfall['TI_filtered'] = [x[0] for x in tis.sample(coords)]
 tis.close()
 
 # add sat val
@@ -298,6 +298,7 @@ dfall['sat_val'] = dfall['Name'].apply(lambda x: sat_val_dict[x])
 # add discharge
 dfnew = dfall.merge(dfqug, on='date', how='left')
 dfnew['Q'].fillna(0.0, inplace=True)
+dfnew.drop(columns=['OID_', 'BeginTime', 'Unnamed: 0', 'FolderPath'], inplace=True)
 
 #%% scatter TI-sat
 
@@ -320,8 +321,96 @@ ax.set_xlabel('TI')
 plt.show()
 
 
-#%% cumulative TI-sat
+#%% logistic regression with statsmodels
 
+# make saturation into a binary field
+dfnew['sat_bin'] = (dfnew['sat_val'] > 0) * 1
+
+# logit using TI and Q as predictors
+model = smf.logit('sat_bin ~ TI_filtered + Q', data=dfnew).fit()
+
+# check model performance
+print(model.summary())
+
+# predict in sample
+in_sample = pd.DataFrame({'prob':model.predict()})
+
+#%% make a sensitivity-specificity plot for model prediction
+
+thresholds = np.linspace(0.2,0.8,20)
+sens = np.zeros_like(thresholds)
+spec = np.zeros_like(thresholds)
+for i, thresh in enumerate(thresholds):
+    in_sample['pred_label'] = (in_sample['prob']>thresh).astype(int)
+    cs = pd.crosstab(in_sample['pred_label'],dfnew['sat_bin'])
+    sens[i] = cs[1][1]/(cs[1][1] + cs[1][0])
+    spec[i] = cs[0][0]/(cs[0][0] + cs[0][1])
+
+
+fig, ax = plt.subplots()
+sc = ax.scatter(1-spec, sens, c=thresholds)
+ax.axline([0,0], [1,1])
+ax.set_xlabel('1-specificity')
+ax.set_ylabel('sensitivity')
+ax.colorbar(sc, label='threshold')
+plt.savefig(save_directory+'sens_spec_DR.png')
+
+#%% predict odds of saturation in sample with the model
+
+dfnew['pred_prob'] = model.predict()
+
+fig, ax = plt.subplots()
+grouped = dfnew.groupby('date')
+for key, group in grouped:
+    ax.scatter(group['TI_filtered'], 
+                group['pred_prob'],
+                c=group['Q'],
+                vmin=min(dfnew['Q']),
+                vmax=max(dfnew['Q']),
+                cmap='plasma',
+                )
+
+
+#%% get all the topographic index values for Druids Run
+
+areafile = "LSDTT/baltimore2015_DR1_d8_area.bil"
+af = rd.open(path+areafile)
+area = af.read(1).astype(float)
+af.close()
+
+TIfile = "LSDTT/baltimore2015_DR1_TIfiltered.tif" # Druids Run
+tif = rd.open(path+TIfile)
+TI = tif.read(1).astype(float)
+TI = TI[area>0] # np.ma.masked_array(TI, mask=area==-9999)
+
+#%% Predict out of sample, and plot with TI CDF
+
+TI1 = np.linspace(1,12,100)
+Q_all = np.linspace(0.01,6,5)
+fig, ax = plt.subplots()
+
+for i, Q in enumerate(Q_all):
+    pred = model.predict(exog=dict(TI_filtered=TI1, Q=Q*np.ones_like(TI1)))
+
+    ax.plot(TI1, pred, color=cm.viridis(Q/max(Q_all)), label='Q=%.2f'%Q)
+ax.axvspan(dfnew['TI_filtered'].min(), dfnew['TI_filtered'].max(), alpha=0.2, color='r')
+ax.set_ylim(-0.05,1.05)
+ax.legend(frameon=False)
+ax.set_xlabel('TI')
+ax.set_ylabel(r'Modeled P$(saturated)$')
+
+ax1 = ax.twinx()
+ax1.plot(np.sort(TI), np.linspace(0,1,len(TI)), color='k', linewidth=1, label='CDF')
+ax1.set_xlim((1,12))
+ax1.set_ylim(-0.05,1.05)
+ax1.set_ylabel(r'P$(TI \leq TI_x)$')
+ax1.legend(frameon=False, loc='lower right')
+plt.savefig(save_directory+'pred_sat_ti_DR.png')
+
+
+#%% ##### other stuff
+
+# cumulative TI-sat
 norm = colors.Normalize(vmin=min(dfnew['Q']), vmax=max(dfnew['Q']))
 
 fig, ax = plt.subplots()
@@ -338,46 +427,3 @@ for key, group in grouped:
 ax.set_ylabel('Proportion Saturated')
 ax.set_xlabel(r'$TI \leq TI_x$')
 plt.savefig(save_directory+'cumulative_sat_DR.png')
-
-# %%
-# logistic regression to determine saturation
-
-# X = df['TI_filtered'].values.reshape(-1, 1)
-
-X = df[['TI_filtered', 'curv']]
-y = df['sat_val'] > 1
-y = y.astype(int)
-
-clf = LogisticRegression(random_state=0).fit(X, y)
-
-pred = clf.predict(X)
-prob = clf.predict_proba(X)
-
-sc = clf.score(X, y)
-
-
-
-# %%
-
-plt.figure()
-plt.scatter(df['TI_filtered'], prob[:,0])
-plt.scatter(df['TI_filtered'], prob[:,1])
-
-# %%
-
-fig, ax = plt.subplots()
-sc = ax.scatter(df['TI_filtered'], 
-                y + 0.05*np.random.randn(len(df)), 
-                c='k',
-                alpha=0.3,
-                )
-sc = ax.scatter(df['TI_filtered'], 
-                pred + 0.05*np.random.randn(len(df)), 
-                c=df['curv'], 
-                cmap='coolwarm', 
-                norm=colors.CenteredNorm())
-
-
-# %%
-df.to_csv(path+'sat_20220419.csv')
-# %%
